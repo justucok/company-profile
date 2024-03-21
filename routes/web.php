285de4ce21d7082e3e54c0bc5d8d1d3f2532d87d
justucok <rahmanyw43@gmail.com>
2024-03-21 23:56:45 +0700

    full commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AppController::class , 'index'] )-> name('index');
+
+Route::get('/about', [AppController::class , 'about'] )-> name('about');
+
+Route::get('/contact', [AppController::class , 'contact'] )-> name('contact');
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
